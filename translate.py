3188ed40dcec50a8e2872402815eb0b31231d2c8
jorrdansoh <sohjingren@gmail.com>
2023-05-16 17:44:35 +0800

    Update script with exclusion handling

--- translate.py
+++ translate.py
@@ -6,9 +6,14 @@ import re
 # 2. Enter the dictionary into ChatGPT along with the prompt.
 # 3. Run this script with `python3 translate.py`
 
+# NOTE: the scanning script is very powerful as it extracted words that are used as types in code.
+# As a result, this script can replace those words with Chinese, causing docker build to fail.
+# Current solution is to enter the words we don't want to be replaced into en-exclusions.json,
+# and the script will exclude them when replacing.
+
 # Paths that need to be scanned
 # Assumes MikoMiko is in the same directory as i18n-pick
-# bin/i18n-pick.js scan ../MikoMiko/src/components ../MikoMiko/src/pages ../MikoMiko/src/server
+# bin/i18n-pick.js scan ../MikoMiko/src/components ../MikoMiko/src/pages
 
 # Export json file to a dict for easy translation
 # bin/i18n-pick.js export
@@ -17,17 +22,21 @@ import re
 # Translate the values to Chinese for me. Don't modify the key. Return the result in code block:
 
 # Define the path to the translated dictionary
-translated_dict_file = 'zh.js'
+translated_dict_file = 'i18n-translation/zh.js'
 
 with open(translated_dict_file) as f:
     translated_dict = json.load(f)
 
 # Define the paths to the English and Chinese JSON files
 original_file = 'i18n-messages/en.json'
-translated_file = 'i18n-messages/zh.json'
+exclusion_file = 'i18n-translation/en-exclusions.json'
+translated_file = 'i18n-translation/zh.json'
 
 with open(original_file) as f:
     original_data = json.load(f)
+
+with open(exclusion_file) as f:
+    excluded_data = str(json.load(f))
 
 for data in original_data:
     if data['id'] not in translated_dict:
@@ -41,6 +50,9 @@ for data in original_data:
     # Iterate over the source locations in the JSON file
     source_locations = data['source']
     for location in source_locations:
+        if data['id'] in excluded_data and location['location'] in excluded_data:
+            continue
+
         file_path = location['location'].split('#')[0]
 
         # If there are no line numbers, find and replace words in the whole file
@@ -67,8 +79,8 @@ for data in original_data:
 
         if line_number_start == line_number_end:
             line = lines[line_number_start - 1]
-            line = line.replace(data['id'], translated_message)
-            lines[line_number_start - 1] = line
+            translated_line = line.replace(data['id'], translated_message)
+            lines[line_number_start - 1] = translated_line
         else:
             is_translated = False
             key = data['id']
@@ -92,9 +104,9 @@ for data in original_data:
                         break
                 
                 replacement = translated_message if not is_translated else ''
-                line = line[:key_start] + replacement + line[key_start + match_len:]
+                translated_line = line[:key_start] + replacement + line[key_start + match_len:]
 
-                lines[line_num] = line
+                lines[line_num] = translated_line
 
                 is_translated = True
                 key = key[match_len:].lstrip()
